ba3e27635662341f0b8dbc916df4ede7ca235a09
lss555 <chrisheibel7@gmail.com>
2021-01-12 17:30:19 -0700

    Home page info sections

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Video from '../../videos/video.mp4';
-import { HeroContainer, HeroBg, VideoBg } from './Hero'
+import { HeroContainer, HeroBg, VideoBg, HeroP, HeroH1, HeroContent, HeroBtnWrapper, ArrowForward, ArrowRight } from './Hero'
+import { Button } from '../ButtonElement.js'
 
 const HeroSection = () => {
+  const [hover, setHover] = useState(false)
+
+  const onHover = () => {
+    setHover(!hover)
+  }
     return (
         <HeroContainer id='home'>
           <HeroBg>
             <VideoBg autoPlay loop muted src={Video} type='video/mp4'/>
           </HeroBg>
+          <HeroContent>
+            <HeroH1>Soar Counseling, Cody, Wyoming</HeroH1>
+            <HeroP></HeroP>
+            <HeroBtnWrapper>
+              <Button to='contact'
+              onMouseEnter={onHover}
+              onMouseLeave={onHover}
+              primary='true'
+              dark='true'
+              >
+                Get in touch {hover ? <ArrowForward /> : <ArrowRight />}
+              </Button>
+            </HeroBtnWrapper>
+          </HeroContent>
         </HeroContainer>
     )
 }
---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,50 @@
+export const homeObjOne = {
+  id: 'about',
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine:'Soar Counseling',
+  headline: 'My mission is to embrace and support indivuals who are struggling with emotional challenges',
+  description: 'Today you are one step closer to a new you where you feel empowered and on a positive and healthy path to growth and well-being.',
+  buttonLabel: 'Send me a message',
+  imgStart: false,
+  img: require('../../images/yellowstone.jpg').default,
+  alt: 'profile image',
+  dark: true,
+  primary: true,
+  darkText: false
+}
+
+export const homeObjTwo = {
+  id: 'faq',
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine:'Soar Blog',
+  headline: 'My mission is to embrace and support indivuals who are struggling with emotional challenges',
+  description: 'Today you are one step closer to a new you where you feel empowered and on a positive and healthy path to growth and well-being.',
+  buttonLabel: 'Go to blog',
+  imgStart: true,
+  img: require('../../images/yellowstone.jpg').default,
+  alt: 'profile image',
+  dark: true,
+  primary: true,
+  darkText: true
+}
+
+export const homeObjThree = {
+  id: 'contact',
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine:'Soar Counseling',
+  headline: 'My mission is to embrace and support indivuals who are struggling with emotional challenges',
+  description: 'Today you are one step closer to a new you where you feel empowered and on a positive and healthy path to growth and well-being.',
+  buttonLabel: 'Send me a message',
+  imgStart: false,
+  img: require('../../images/yellowstone.jpg').default,
+  alt: 'profile image',
+  dark: true,
+  primary: true,
+  darkText: false
+}
