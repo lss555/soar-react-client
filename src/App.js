85324b89588bc140cf0c9652203b38514fc80055
lss555 <chrisheibel7@gmail.com>
2021-01-20 10:53:18 -0700

    FAQ page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Home from './pages'
 import SignIn from './pages/Sign-In'
+import Faq from './components/Faq/Faq.js'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Switch>
         <Route path='/' component={Home} exact />
         <Route path='/sign-in' component={SignIn} exact />
+        <Route path='/faq' component={Faq} exact />
       </Switch>
     </Router>
   );
